3aed12ec2c3770b594ad8d16ea79e0804dbff0ee
ChoiHyeongu <a11dsd123@gmail.com>
2019-04-20 22:32:31 +0900

    Detail

--- app/src/main/java/songpatechnicalhighschool/motivation/ctlw/Adapter/PostAdapter.java
+++ app/src/main/java/songpatechnicalhighschool/motivation/ctlw/Adapter/PostAdapter.java
@@ -1,7 +1,9 @@
 package songpatechnicalhighschool.motivation.ctlw.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +13,7 @@ import android.widget.TextView;
 import java.util.List;
 
 import songpatechnicalhighschool.motivation.ctlw.Module.Post;
+import songpatechnicalhighschool.motivation.ctlw.PostDetailActivity;
 import songpatechnicalhighschool.motivation.ctlw.R;
 
 public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
@@ -36,7 +39,12 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
         holder.titleText.setText(post.getTitle());
         holder.dateText.setText(post.getDate());
         holder.previewText.setText(post.getPreview());
-
+        holder.postLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                context.startActivity(new Intent(context, PostDetailActivity.class));
+            }
+        });
     }
 
     @Override
@@ -49,6 +57,7 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
         public TextView titleText;
         public TextView dateText;
         public TextView previewText;
+        public CardView postLayout;
         public TextView content;
 
         public ViewHolder(View itemView) {
@@ -56,6 +65,7 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
             titleText = itemView.findViewById(R.id.post_title_text);
             dateText = itemView.findViewById(R.id.post_time_text);
             previewText = itemView.findViewById(R.id.post_preview_text);
+            postLayout = itemView.findViewById(R.id.post_post_layout);
         }
     }
 }
